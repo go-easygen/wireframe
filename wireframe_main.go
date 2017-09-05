a9e85c8854526eef8decb9568c709e4bd4e61ddb
Tong Sun <suntong@cpan.org>
2017-09-04 21:14:26 -0400

    - [+] add all the support functions

--- wireframe_main.go
+++ wireframe_main.go
@@ -11,9 +11,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/labstack/gommon/color"
 	"github.com/mkideal/cli"
 )
+
+////////////////////////////////////////////////////////////////////////////
+// Constant and data type/structure definitions
+
+// The OptsT type defines all the configurable options from cli.
+type OptsT struct {
+	Verbose int
+}
 
 ////////////////////////////////////////////////////////////////////////////
 // Global variables definitions
@@ -22,9 +32,11 @@ var (
 	progname = "wireframe"
 	version  = "0.1.0"
 	date     = "2017-09-04"
-)
 
-var rootArgv *rootT
+	rootArgv *rootT
+	// Opts store all the configurable options
+	Opts OptsT
+)
 
 ////////////////////////////////////////////////////////////////////////////
 // Function definitions
@@ -51,4 +63,58 @@ func wireframe(ctx *cli.Context) error {
 	fmt.Println()
 
 	return nil
+}
+
+// SUPPORT-FUNCTIONS
+//==========================================================================
+// support functions
+
+// Abs returns the absolute value of x.
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// Basename returns the file name without extension.
+func Basename(s string) string {
+	n := strings.LastIndexByte(s, '.')
+	if n > 0 {
+		return s[:n]
+	}
+	return s
+}
+
+// IsExist checks if the given file exist
+func IsExist(fileName string) bool {
+	_, err := os.Stat(fileName)
+	return err == nil || os.IsExist(err)
+}
+
+func warning(m string) {
+	fmt.Fprintf(os.Stderr, "[%s] %s: %s\n", progname, color.Yellow("Warning"), m)
+}
+
+func warnOn(errCase string, e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "[%s] %s, %s: %v\n",
+			color.White(progname), color.Yellow("Error"), errCase, e)
+	}
+}
+
+// abortOn will quit on anticipated errors gracefully without stack trace
+func abortOn(errCase string, e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "[%s] %s, %s: %v\n",
+			color.White(progname), color.Red("Error"), errCase, e)
+		os.Exit(1)
+	}
+}
+
+// verbose will print info to stderr according to the verbose level setting
+func verbose(levelSet int, format string, args ...interface{}) {
+	if Opts.Verbose >= levelSet {
+		fmt.Fprintf(os.Stderr, "["+progname+"] "+format+"\n", args...)
+	}
 }
